feadbf121ea7dbb1ea3dbf1b5a6fbff5f5217f7f
savifon <savifon1@gmail.com>
2022-02-27 17:45:21 -0300

    faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -12,6 +16,24 @@ export default NextAuth({
         },
       },
     }),
-    
   ],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        const { email } = user;
+                  
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: email }
+          )
+        )
+      
+        return true
+      } catch(err) {
+        console.log(err)
+        return false
+      }      
+    }
+  }
 })
